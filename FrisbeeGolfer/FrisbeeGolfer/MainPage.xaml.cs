8923e65108836d9738562cebc26fbf2f9fda44ee
BendikSim <Bendiksim98@gmail.com>
2021-04-09 16:40:37 +0200

    Add project files.

---
+++ FrisbeeGolfer/FrisbeeGolfer/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FrisbeeGolfer
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void Nr_Clicked(object sender, System.EventArgs e)
+        {
+
+        }
+
+        void P_Clicked(object sender, System.EventArgs e)
+        {
+
+        }
+        void L_Clicked(object sender, System.EventArgs e)
+        {
+
+        }
+        void Au_Clicked(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
